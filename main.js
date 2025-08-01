f131965614cdd95f79ecab9b9285521d59bc1213
yamila_rengifo <yamirngifo18@gmail.com>
2025-08-01 17:33:39 -0300

    yami

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const menuBtn = document.getElementById("menuBtn");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
 let paginaActual = 1;
 
 
